4e1653eac5f41947f5f404157b4b13ac4cc62618
Nick Aliferov <x8core@gmail.com>
2025-03-06 13:33:22 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 const runFrontend = async (x) => {
 
-  x.s('docMkElement', (x) => {
-    const { id, tag, txt, html, events, css, attrs, attributes } = x
-
-    const o = document.createElement(tag || 'div')
-    if (id) o.id = id
-    if (x['class'])
-      o.className = Array.isArray(x['class'])
-        ? x['class'].join(' ')
-        : x['class']
-    if (txt) o.innerText = txt
-    if (html) o.innerHTML = html
-    if (css) for (let k in css) o.style[k] = css[k]
-    if (attributes) for (let k in attributes) o.setAttribute(k, attributes[k])
-    if (attrs) for (let k in attrs) o.setAttribute(k, attrs[k])
-    if (events) for (let k in events) o.addEventListener(k, events[k])
-
-    return o
-  })
-
   x.s('oFactory', async (x) => {
     const state = x.state
     const isObj = (o) => typeof o === 'object' && o !== null
@@ -80,6 +61,7 @@ const runFrontend = async (x) => {
   x.s('getDomById', async (x) => document.getElementById(x.id))
 
 
+  
   const objects = {}
 
   x.s('getObject', (x) => objects[x.id])
